c03b7debacbf6d4ff1eda05d1abcbe0cff25af5e
Kuldeep Dhillon <kuldeepd@students.federation.edu.au>
2025-04-29 23:10:43 +1000

    transfer method test is done

--- src/lab7/BankAccount.java
+++ src/lab7/BankAccount.java
@@ -32,6 +32,25 @@ public class BankAccount {
 		return true;
 	}
 	
+	public void transfer(BankAccount fromAccount, double amount) {
+	    // Check: source and destination must not be the same
+	    if (fromAccount == this) {
+	        System.out.println("Cannot transfer to the same account.");
+	        return;
+	    }
+
+	    // Try to withdraw from the source account
+	    boolean success = fromAccount.withdraw(amount);
+
+	    if (success) {
+	        // Deposit into this account
+	        this.deposit(amount);
+	        System.out.println("Transfer successful.");
+	    } else {
+	        System.out.println("Transfer failed: insufficient funds.");
+	    }
+	}
+
 	public double getBalance() {
 		return balance;
 	}
